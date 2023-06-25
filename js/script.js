1e9b5157090a736de524344012e1e3c055346ddb
7pigy7 <7pigy7@gmail.com>
2023-06-24 19:22:28 -0700

    start

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//load header and footer?
+function loadHeaderandFooter(){
+ //  <image src="/images/Logo.png" alt="Queen's Games Logo"></image>
+   // <nav>
+     //   <ul>
+       //     <li><a href="index.html">Home</a></li>
+         //   <li>If Adventure</li>
+           // <li>Number Guess</li>
+            //<li>Hangman</li>
+       // </ul>
+   // </nav>
+   return;
+}
+//template for text game?
