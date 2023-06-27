84cdc6446e01485b2c29449fdffbf18d8469c2e7
7pigy7 <7pigy7@gmail.com>
2023-06-27 14:38:55 -0700

    CSS and Home finished

--- js/script.js
+++ js/script.js
@@ -2,8 +2,3 @@ import { loadHeaderFooter } from "./utils.mjs";
 //load header and footer?
 loadHeaderFooter();
 //template for text game?
-
-document.addEventListener("DOMContentLoaded", function() { 
-    //update the last modified text
-    document.getElementById("last-modified").innerHTML = document.lastModified;
-  });
